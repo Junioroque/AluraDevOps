f9efbde61f70f39469d63228a0bcd8861ff87bdc
Junio Roque <jcjunioroque@gmail.com>
2025-11-23 22:27:36 -0300

    desafio 03

---
+++ javascript/semana_2/funcao/exercicios/desafio_03/script.js
@@ -0,0 +1,11 @@
+function mensagem() {
+    let numero = document.getElementById("numero").value;
+    let resultado = document.getElementById("resultado");
+
+    resultado.innerHTML = "";
+    
+    resultado.innerHTML = `O doblo: ${nome}!`;   
+    console.log(`O doblo: ${numero}}!`); 
+}
+
+
